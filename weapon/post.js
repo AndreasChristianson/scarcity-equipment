fe0f699632f255380e406755a95de523cce5cc2b
Andreas Christianson <sullage@pessimistic-it.com>
2019-12-03 16:17:02 +1300

    added git sha to lambda env

--- weapon/post.js
+++ weapon/post.js
@@ -1,5 +1,8 @@
-exports.post = async (event, context) => ({
+exports.handler = async (event, context) => ({
   statusCode: 200,
+  headers: {
+    version: process.env.GIT_COMMIT_SHORT
+  },
   body: JSON.stringify({
     message: 'hello',
     event,
--- weapon/post.spec.js
+++ weapon/post.spec.js
@@ -1,8 +1,20 @@
-const { post } = require('./post')
+const { handler } = require('./post')
+const { Chance } = require('chance')
 
 describe('post', () => {
+  const chance = new Chance()
+
+  beforeEach(() => {
+    process.env.GIT_COMMIT_SHORT = chance.hash()
+  })
+
   it('should return 200', async () => {
-    const response = await post()
+    const response = await handler()
     expect(response.statusCode).toBe(200)
+  })
+
+  it('should set the version header', async () => {
+    const response = await handler()
+    expect(response.headers.version).toBe(process.env.GIT_COMMIT_SHORT)
   })
 })
