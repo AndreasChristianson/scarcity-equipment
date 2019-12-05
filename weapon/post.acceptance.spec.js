75f98fe82753fccab8d1e0b7a61dc0bad87b4957
Andreas Christianson <sullage@pessimistic-it.com>
2019-12-05 22:34:43 +1300

    fixing ATs

--- weapon/post.acceptance.spec.js
+++ weapon/post.acceptance.spec.js
@@ -1,6 +1,20 @@
+const Chance = require('chance')
 const frisby = require('frisby')
 
-it('should have the right version', async () => {
-  const res = await frisby.post(`${process.env.host}/equipment/weapon`)
-  expect(res.headers.get('version')).toBe(process.env.TRAVIS_COMMIT)
+describe('weapon POST', () => {
+  const chance = new Chance()
+  let payload
+
+  beforeEach(() => {
+    payload = {
+      id: chance.guid(),
+      itemLevel: chance.d100()
+    }
+  })
+
+  it('should have the right version', async () => {
+    const res = await frisby.post(`${process.env.host}/equipment/weapon`, payload)
+    expect(res.status).toBe(200)
+    expect(res.headers.get('version')).toBe(process.env.TRAVIS_COMMIT)
+  })
 })
